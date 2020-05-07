89f620f01c51f560e6941c859f3e32b81e48a14a
snokia5700 <64929925+snokia5700@users.noreply.github.com>
2020-05-06 23:11:51 -0400

    Create ip.php
    Class that includes basic functions required in the script

---
+++ ip.php
@@ -0,0 +1,68 @@
+<?php
+public class ip {
+  int $seg_one;
+  int $seg_two;
+  int $seg_three;
+  int $seg_four;
+  int $subnet_mask;
+  string $class;
+  
+  /*
+   * @param string ip
+   * @param int mask
+   * @return bool
+   */
+  public function __construct(string $ip, int $mask) {
+    $ip_array = explode(".", $ip);
+    $this->seg_one = $ip_array[0];
+    $this->seg_two = $ip_array[1];
+    $this->seg_three = $ip_array[2];
+    $this->seg_four = $ip_array[3];
+    $this->subnet_mask = $mask;
+    
+    if (validate()) {
+      $this->class = class_detect();
+      return true;
+    }
+    
+    return false;
+  }
+  
+  /*
+   * @return bool
+   */
+  bool public function validate() {
+    if (seg_check($this->seg_one) && seg_check($this->seg_two) && seg_check($this->seg_three) && seg_check($this->seg_four)
+      && $this->subnet_mask >= 1 && $this->subnet_mask <= 255) {
+      return true;
+    }
+    return false;
+  }
+  
+  /*
+   * @return string
+   */
+  string public function class_detect() {
+    if ($this->subnet_mask <= 8) {
+      return "A";
+    } elseif ($this->subnet_mask <= 16) {
+      return "B";
+    } elseif ($this->subnet_mask <= 24) {
+      return "C";
+    } else {
+      return "D";
+    }
+  }
+  
+  /*
+   * @param int segment
+   * @return bool
+   */
+  bool private function seg_check($segment) {
+    if ($segment >= 0 && $segment <= 255) {
+      return true;
+    }
+    return false;
+  }
+}
+?>
