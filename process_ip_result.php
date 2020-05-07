44f244bb02805123a18b38754e1d56229f64bf48
snokia5700 <64929925+snokia5700@users.noreply.github.com>
2020-05-07 12:38:42 -0400

    Updated process_ip_result.php
    Uses function to display result

--- process_ip_result.php
+++ process_ip_result.php
@@ -1,13 +1,22 @@
 <?php
 
-if (!isset($_POST['ipv4']) && !isset($_POST['mask'])) {
-  echo "Either value not set. Please try again.";
-} else {
-  $ip_object = new ip($_POST['ipv4'], $_POST['mask']);
-  if ($ip_object === false) {
-    echo "Either value is invalid. Please try again.";
+var $error = null;
+
+function display_result() {
+  if (!isset($_POST['ipv4']) && !isset($_POST['mask'])) {
+    $error = "Either value is not set. Please try again.\n";
   } else {
-    $class = $ip_object->class;
+    $ip_object = new ip($_POST['ipv4'], $_POST['mask']);
+    if ($ip_object === false) {
+      $error = "Either value is invalid. Please try again.";
+    } else {
+      echo "Class is: " . $ip_object->class . "\n";
+      echo "It is private.\n";
+    }
+  }
+  
+  if (!isnull($error)) {
+    echo $error;
   }
 }
 
